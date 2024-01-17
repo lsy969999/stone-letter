e06a2a067a186c11e8817db5d6e2b72280bbab83
lsy969999 <lsy969999@gmail.com>
2024-01-17 20:21:15 +0900

    add secret

--- ios/stone-letter/Constant.swift
+++ ios/stone-letter/Constant.swift
@@ -9,4 +9,6 @@ import Foundation
 
 struct Const {
     static let TEST = Bundle.main.object(forInfoDictionaryKey: "TEST") as! String
+    static let FIREBASE_OPTIONS_PATH = Bundle.main.object(forInfoDictionaryKey: "FIREBASE_OPTIONS_PATH") as! String
+    static let ENV = Bundle.main.object(forInfoDictionaryKey: "ENV") as! String
 }
--- ios/stone-letter/stone_letterApp.swift
+++ ios/stone-letter/stone_letterApp.swift
@@ -10,16 +10,10 @@ import FirebaseCore
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-      #if DEBUG
-          let filePath = Bundle.main.path(forResource: "GoogleService-Info.debug", ofType: "plist")!;
-          let options = FirebaseOptions.init(contentsOfFile: filePath)!
-          FirebaseApp.configure(options: options)
-      #else
-          let filePath = Bundle.main.path(forResource: "GoogleService-Info.release", ofType: "plist")!;
-          let options = FirebaseOptions.init(contentsOfFile: filePath)!
-          FirebaseApp.configure(options: options)
-      #endif
-      print("TEST: \(Const.TEST)")
+      let filePath = Bundle.main.path(forResource: Const.FIREBASE_OPTIONS_PATH, ofType: "plist")!;
+      let options = FirebaseOptions.init(contentsOfFile: filePath)!
+      FirebaseApp.configure(options: options)
+      print("TEST: \(Const.TEST), ENV: \(Const.ENV)")
       
     
     return true
